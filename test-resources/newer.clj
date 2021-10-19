99b9e9cfd8c34fe13a783040cc0535061072542f
lread <lee@dlread.com>
2021-10-19 16:20:58 -0400

    Private vars are now excluded from API diff
    Closes #8

--- test-resources/newer.clj
+++ test-resources/newer.clj
@@ -1,5 +1,12 @@
 (ns example)
 
+;; privates should be ignored
+(defn- private-fn [x y])
+(def ^:private private-defb 72)
+
+;; this was public, should appear as removed
+(defn- becomes-private [])
+
 (def x 1)
 ;; (def y 2)
 (def ^:deprecated z 3)
